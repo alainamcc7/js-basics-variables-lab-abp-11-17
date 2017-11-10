a12b35e5e008f8b803d92ed29f35814b1ed8880c
Alaina Mccullough <alainamccullough@live.com>
2017-11-10 22:25:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
+  let('companyName', function(){
+    it('is set as Scuber', function(){
+      expect(companyName).to.equal('Scuber');
+    });
